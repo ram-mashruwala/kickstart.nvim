487bd5c3b532092c04296b4ecc1dd044706e1775
Ram Mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2026-01-31 16:50:24 -0800

    Fixed Treesitter text objects and added a few packages

--- init.lua
+++ init.lua
@@ -83,7 +83,6 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -95,7 +94,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -421,6 +420,8 @@ require('lazy').setup({
         defaults = {
           file_ignore_patters = {
             'migrations',
+            'venv',
+            '__pycache__',
           },
         },
         -- pickers = {}
@@ -920,7 +921,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-frappe'
     end,
   },
   {
@@ -998,7 +999,7 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
@@ -1021,11 +1022,9 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    dependencies = {},
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
@@ -1056,97 +1055,97 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
-      textobjects = {
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = { query = '@class.outer', desc = 'Next class start' },
-            --
-            -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-            [']o'] = '@loop.*',
-            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-            --
-            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-            [']s'] = { query = '@local.scope', query_group = 'locals', desc = 'Next scope' },
-            [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-          -- Below will go to either the start or the end, whichever is closer.
-          -- Use if you want more granular movements
-          -- Make it even more gradual by adding multiple queries and regex.
-          goto_next = {
-            [']d'] = '@conditional.outer',
-          },
-          goto_previous = {
-            ['[d'] = '@conditional.outer',
-          },
-        },
-        lsp_interop = {
-          enable = true,
-          border = 'none',
-          floating_preview_opts = {},
-          peek_definition_code = {
-            ['<leader>df'] = '@function.outer',
-            ['<leader>dF'] = '@class.outer',
-          },
-        },
-        select = {
-          enable = true,
-
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['af'] = { query = '@function.outer', desc = 'Select inner part of a function region' },
-            ['if'] = { query = '@function.inner', desc = 'Select outter part of a function region' },
-            ['ai'] = { query = '@conditional.outer', desc = 'Select inner part of a conditional region' },
-            ['ii'] = { query = '@conditional.inner', desc = 'Select outter part of a conditional region' },
-            ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class region' },
-            -- You can optionally set descriptions to the mappings (used in the desc parameter of
-            -- nvim_buf_set_keymap) which plugins like which-key display
-            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-            -- You can also use captures from other query groups like `locals.scm`
-            ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
-          },
-          -- You can choose the select mode (default is charwise 'v')
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * method: eg 'v' or 'o'
-          -- and should return the mode ('v', 'V', or '<c-v>') or a table
-          -- mapping query_strings to modes.
-          selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V', -- linewise
-            ['@class.outer'] = '<c-v>', -- blockwise
-          },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true or false
-          include_surrounding_whitespace = true,
-        },
-      },
+      -- textobjects = {
+      --   move = {
+      --     enable = true,
+      --     set_jumps = true, -- whether to set jumps in the jumplist
+      --     goto_next_start = {
+      --       [']m'] = '@function.outer',
+      --       [']]'] = { query = '@class.outer', desc = 'Next class start' },
+      --       --
+      --       -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+      --       [']o'] = '@loop.*',
+      --       -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+      --       --
+      --       -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+      --       -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+      --       [']s'] = { query = '@local.scope', query_group = 'locals', desc = 'Next scope' },
+      --       [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+      --     },
+      --     goto_next_end = {
+      --       [']M'] = '@function.outer',
+      --       [']['] = '@class.outer',
+      --     },
+      --     goto_previous_start = {
+      --       ['[m'] = '@function.outer',
+      --       ['[['] = '@class.outer',
+      --     },
+      --     goto_previous_end = {
+      --       ['[M'] = '@function.outer',
+      --       ['[]'] = '@class.outer',
+      --     },
+      --     -- Below will go to either the start or the end, whichever is closer.
+      --     -- Use if you want more granular movements
+      --     -- Make it even more gradual by adding multiple queries and regex.
+      --     goto_next = {
+      --       [']d'] = '@conditional.outer',
+      --     },
+      --     goto_previous = {
+      --       ['[d'] = '@conditional.outer',
+      --     },
+      --   },
+      --   lsp_interop = {
+      --     enable = true,
+      --     border = 'none',
+      --     floating_preview_opts = {},
+      --     peek_definition_code = {
+      --       ['<leader>df'] = '@function.outer',
+      --       ['<leader>dF'] = '@class.outer',
+      --     },
+      --   },
+      --   select = {
+      --     enable = true,
+      --
+      --     -- Automatically jump forward to textobj, similar to targets.vim
+      --     lookahead = true,
+      --
+      --     keymaps = {
+      --       -- You can use the capture groups defined in textobjects.scm
+      --       ['af'] = { query = '@function.outer', desc = 'Select inner part of a function region' },
+      --       ['if'] = { query = '@function.inner', desc = 'Select outter part of a function region' },
+      --       ['ai'] = { query = '@conditional.outer', desc = 'Select inner part of a conditional region' },
+      --       ['ii'] = { query = '@conditional.inner', desc = 'Select outter part of a conditional region' },
+      --       ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class region' },
+      --       -- You can optionally set descriptions to the mappings (used in the desc parameter of
+      --       -- nvim_buf_set_keymap) which plugins like which-key display
+      --       ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+      --       -- You can also use captures from other query groups like `locals.scm`
+      --       ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
+      --     },
+      --     -- You can choose the select mode (default is charwise 'v')
+      --     --
+      --     -- Can also be a function which gets passed a table with the keys
+      --     -- * query_string: eg '@function.inner'
+      --     -- * method: eg 'v' or 'o'
+      --     -- and should return the mode ('v', 'V', or '<c-v>') or a table
+      --     -- mapping query_strings to modes.
+      --     selection_modes = {
+      --       ['@parameter.outer'] = 'v', -- charwise
+      --       ['@function.outer'] = 'V', -- linewise
+      --       ['@class.outer'] = '<c-v>', -- blockwise
+      --     },
+      --     -- If you set this to `true` (default is `false`) then any textobject is
+      --     -- extended to include preceding or succeeding whitespace. Succeeding
+      --     -- whitespace has priority in order to act similarly to eg the built-in
+      --     -- `ap`.
+      --     --
+      --     -- Can also be a function which gets passed a table with the keys
+      --     -- * query_string: eg '@function.inner'
+      --     -- * selection_mode: eg 'v'
+      --     -- and should return true or false
+      --     include_surrounding_whitespace = true,
+      --   },
+      -- },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
@@ -1154,6 +1153,44 @@ require('lazy').setup({
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+
+      -- Or, disable per filetype (add as you like)
+      -- vim.g.no_python_maps = true
+      -- vim.g.no_ruby_maps = true
+      -- vim.g.no_rust_maps = true
+      -- vim.g.no_go_maps = true
+    end,
+    config = function()
+      require('nvim-treesitter-textobjects').setup {}
+
+      vim.keymap.set({ 'x', 'o' }, 'af', function()
+        require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects')
+      end)
+      vim.keymap.set({ 'x', 'o' }, 'if', function()
+        require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects')
+      end)
+      vim.keymap.set({ 'x', 'o' }, 'ai', function()
+        require('nvim-treesitter-textobjects.select').select_textobject('@conditional.outer', 'textobjects')
+      end)
+      vim.keymap.set({ 'x', 'o' }, 'ii', function()
+        require('nvim-treesitter-textobjects.select').select_textobject('@conditional.inner', 'textobjects')
+      end)
+      vim.keymap.set({ 'x', 'o' }, 'ac', function()
+        require('nvim-treesitter-textobjects.select').select_textobject('@class.outer', 'textobjects')
+      end)
+      vim.keymap.set({ 'x', 'o' }, 'ic', function()
+        require('nvim-treesitter-textobjects.select').select_textobject('@class.inner', 'textobjects')
+      end)
+    end,
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
@@ -1171,6 +1208,35 @@ require('lazy').setup({
   {
     'mbbill/undotree',
   },
+  {
+    'xiyaowong/transparent.nvim',
+  },
+  {
+    'adelarsq/image_preview.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('image_preview').setup()
+    end,
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
+
+  {
+    'kylechui/nvim-surround',
+    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
